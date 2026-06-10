b773d0da2bba940f60717e0c10f55362f8c7e8ba
PrathikshaAM <prathikshaam12@gmail.com>
2026-06-10 16:52:57 +0530

    Add files via upload

---
+++ Assignment 5/defines.v
@@ -0,0 +1,23 @@
+`define MVR 4'b0000
+`define LDB 4'b0001
+`define STB 4'b0010
+`define RDS 4'b0011
+
+//ALU_instruction_code
+`define NOT 4'b1000
+`define AND 4'b1001
+`define OR 4'b1010
+`define ADD 4'b1011
+`define SUB 4'b1100
+`define XOR 4'b1101
+`define INC 4'b1110
+
+//ALU_Opcodes
+`define A_NOT 3'b000
+`define A_AND 3'b001
+`define A_OR 3'b010
+`define A_ADD 3'b011
+`define A_SUB 3'b100
+`define A_XOR 3'b101
+`define A_INC 3'b110
+
